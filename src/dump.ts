fa028da3979b3102f8f7fdddd5250e530ed5ca5c
mkucharz <m@kucharz.net>
2018-09-17 19:35:06 +0200

    feat(): add objects and query arguments

--- src/dump.ts
+++ src/dump.ts
@@ -3,15 +3,35 @@ import json2csv from 'json2csv'
 
 interface Args {
   className: string
+  objects: number[]
+  query: string[]
 }
 
 class Endpoint extends S.Endpoint {
   async run(
-    {response, data}: S.Core,
+    {response, data, users}: S.Core,
     {args}: S.Context<Args>
   ) {
-    const resp = await data[args.className].take(500).list()
-    response(json2csv.parse(resp), 200, 'text/csv')
+    let resp
+
+    if (args.className === 'user') {
+      resp = users.take(500)
+    } else {
+      resp = data[args.className].take(500)
+    }
+
+    if (args.query) {
+      resp.where(...args.query)
+    }
+
+    if (args.objects && args.objects.length > 0) {
+      resp = await resp.find(args.objects)
+    } else {
+      resp = await resp.list()
+    }
+
+    const responseData = resp.length !== 0 ? json2csv.parse(resp) : ''
+    return response(responseData, 200, 'text/csv')
   }
 }
 
